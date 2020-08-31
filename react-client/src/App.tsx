89c0eec2b513e87304f6df43da1abd05a6ae9987
aijialex <aiji.alexhan@gmail.com>
2020-08-31 15:50:09 +0900

    react-markdown test

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactMarkdown from "react-markdown/with-html";
 
 function App() {
-  return <div className="App">App</div>;
+  const [markdown, setMarkdown] = React.useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        "https://raw.githubusercontent.com/aijinet/bodoc-claim-contents/master/contents/example/test1.md"
+      );
+      setMarkdown(await result.text());
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        <ReactMarkdown source={markdown} escapeHtml={false} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
